b6c8506d5111eee85c6159c1aa44900243ee8501
Mikkel Jensen <MikkelJensen199726@hotmail.com>
2022-02-21 15:55:50 +0100

    Small changes to algo Added tests

--- ImprovedBullyTests/test_ImprovedBullyTest.py
+++ ImprovedBullyTests/test_ImprovedBullyTest.py
@@ -27,13 +27,90 @@ class MyTestCase(unittest.TestCase):
         print(p3.whoseLeader)
         print(p4.whoseLeader)
 
-    def starterNodeIsHighest_becomesLeader(self):
-        pass
-    def starterNodeIsLowest_highestBecomeLeader(self):
-        pass
+    def test_starterNodeIsHighest_becomesLeader(self):
+        ListOfP = []
 
-    def highestNodeIsDead_secondHighestBecomeLeader(self):
-        pass
+        p1 = ImprovedBullyProcess(10)
+        ListOfP.append(p1)
+        p2 = ImprovedBullyProcess(9)
+        ListOfP.append(p2)
+        p3 = ImprovedBullyProcess(11)
+        ListOfP.append(p3)
+        p4 = ImprovedBullyProcess(2)
+        ListOfP.append(p4)
+        p5 = ImprovedBullyProcess(4)
+        ListOfP.append(p5)
+
+        p1.setNeighbors(ListOfP)
+        p2.setNeighbors(ListOfP)
+        p3.setNeighbors(ListOfP)
+        p4.setNeighbors(ListOfP)
+        p5.setNeighbors(ListOfP)
+
+        p3.startElection()
+
+        self.assertTrue(p1.whoseLeader == p3.id)
+        self.assertTrue(p2.whoseLeader == p3.id)
+        self.assertTrue(p3.whoseLeader == p3.id)
+        self.assertTrue(p4.whoseLeader == p3.id)
+        self.assertTrue(p5.whoseLeader == p3.id)
+
+    def test_starterNodeIsLowest_highestBecomeLeader(self):
+        ListOfP = []
+
+        p1 = ImprovedBullyProcess(3)
+        ListOfP.append(p1)
+        p2 = ImprovedBullyProcess(12)
+        ListOfP.append(p2)
+        p3 = ImprovedBullyProcess(4)
+        ListOfP.append(p3)
+        p4 = ImprovedBullyProcess(1)
+        ListOfP.append(p4)
+        p5 = ImprovedBullyProcess(5)
+        ListOfP.append(p5)
+
+        p1.setNeighbors(ListOfP)
+        p2.setNeighbors(ListOfP)
+        p3.setNeighbors(ListOfP)
+        p4.setNeighbors(ListOfP)
+        p5.setNeighbors(ListOfP)
+
+        p4.startElection()
+
+        self.assertTrue(p1.whoseLeader == p2.id)
+        self.assertTrue(p2.whoseLeader == p2.id)
+        self.assertTrue(p3.whoseLeader == p2.id)
+        self.assertTrue(p4.whoseLeader == p2.id)
+        self.assertTrue(p5.whoseLeader == p2.id)
+
+    def test_highestNodeIsDead_secondHighestBecomeLeader(self):
+        ListOfP = []
+
+        p1 = ImprovedBullyProcess(3)
+        ListOfP.append(p1)
+        p2 = ImprovedBullyProcess(12)
+        ListOfP.append(p2)
+        p3 = ImprovedBullyProcess(4)
+        ListOfP.append(p3)
+        p4 = ImprovedBullyProcess(1)
+        ListOfP.append(p4)
+        p5 = ImprovedBullyProcess(5)
+        ListOfP.append(p5)
+
+        p1.setNeighbors(ListOfP)
+        p2.setNeighbors(ListOfP)
+        p3.setNeighbors(ListOfP)
+        p4.setNeighbors(ListOfP)
+        p5.setNeighbors(ListOfP)
+
+        p2.killNode()
+
+        p4.startElection()
+
+        self.assertTrue(p1.whoseLeader == p5.id)
+        self.assertTrue(p3.whoseLeader == p5.id)
+        self.assertTrue(p4.whoseLeader == p5.id)
+        self.assertTrue(p5.whoseLeader == p5.id)
 
     def aNodeIsDead_DoesNotLearnLeader(self):
         pass
