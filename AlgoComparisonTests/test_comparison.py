cbe17dcffc7e08e693f7ca89a61738bde15e8247
tobiassandoelund <tobiassandoelund@gmail.com>
2022-03-04 09:16:05 +0100

    Start algo comparison tests

---
+++ AlgoComparisonTests/test_comparison.py
@@ -0,0 +1,26 @@
+import unittest
+from ImprovedBullyProcess import ImprovedBullyProcess
+from OriginalBullyProcess import OriginalBullyProcess
+
+class ComparisonCases(unittest.TestCase):
+    def test_original_10_nodes_none_dead(self):
+        pass
+
+    def test_improved_10_nodes_none_dead(self):
+        listOfP = []
+        networkMessagesSent = [0]
+
+        for i in range(1,10):
+            process = ImprovedBullyProcess(i, networkMessagesSent)
+            listOfP.append(process)
+
+        for p in listOfP:
+            p.setNeighbors(listOfP)
+
+        listOfP[0].startElection()
+
+        for p in listOfP:
+            print(f"Process with id: {p.id} says leader is: {p.whoseLeader}")
+        
+        print(f"Messages Sent in Network: {networkMessagesSent[0]}")
+        
