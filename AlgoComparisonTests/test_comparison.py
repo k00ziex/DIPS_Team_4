9e05eaa94fd476da7187c33624801e37f0b8e5e5
AndreasV <201705136@post.au.dk>
2022-03-04 09:47:25 +0100

    ShuffledList in comparison of algorithms

--- AlgoComparisonTests/test_comparison.py
+++ AlgoComparisonTests/test_comparison.py
@@ -1,16 +1,17 @@
 import unittest
+import random
 from ImprovedBullyProcess import ImprovedBullyProcess
 from OriginalBullyProcess import OriginalBullyProcess
 
 class ComparisonCases(unittest.TestCase):
-    def test_original_10_nodes_none_dead(self):
+    def test_Original_10AliveNodes(self):
         listOfP = []
         networkMessagesSent = [0]
 
         for i in range(1,10):
             process = OriginalBullyProcess(i, networkMessagesSent)
             listOfP.append(process)
-
+                
         for p in listOfP:
             p.setOtherProcessIDs(listOfP)
 
@@ -31,6 +32,27 @@ class ComparisonCases(unittest.TestCase):
 
         for p in listOfP:
             p.setNeighbors(listOfP)
+
+        listOfP[0].startElection()
+
+        for p in listOfP:
+            print(f"Process with id: {p.id} says leader is: {p.whoseLeader}")
+        
+        print(f"Messages Sent in Network: {networkMessagesSent[0]}")
+
+
+    def test_Original_10AliveNodes_ShuffledListOfProcesses(self):
+        listOfP = []
+        networkMessagesSent = [0]
+
+        for i in range(1,10):
+            process = OriginalBullyProcess(i, networkMessagesSent)
+            listOfP.append(process)
+        
+        random.Random(42).shuffle(listOfP) # We seed Random with 42 to get the same randomness every time
+
+        for p in listOfP:
+            p.setOtherProcessIDs(listOfP)
 
         listOfP[0].startElection()
 
